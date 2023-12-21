165581869e71f95c2cc8dacce8f22a82c3ba7db7
Upendra Nayanajith <86357888+upendranayanajith@users.noreply.github.com>
2023-12-21 11:11:00 +0530

    configure

---
+++ myapp00/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My First App</h1>
+        <button className="users-button">Users</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
